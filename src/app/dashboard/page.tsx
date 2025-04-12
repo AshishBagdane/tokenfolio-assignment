fa6744f339c60a37ad5ca60b6227b75ae138f07e
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-04-12 12:52:51 +0530

    feat: improve dashboard page layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 import data from "./data.json";
+import RecentActivity from "@/components/recent-activity";
 
 export default function Page() {
   return (
@@ -21,7 +22,14 @@ export default function Page() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
-              <DataTable data={data} />
+            </div>
+            <div className="flex flex-col gap-4 py-4 mx-auto md:flex-row md:gap-6 md:py-6">
+              <div className="w-full col-span-8">
+                <DataTable data={data} />
+              </div>
+              <div className="w-full col-span-4">
+                <RecentActivity />
+              </div>
             </div>
           </div>
         </div>
